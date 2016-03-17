04d4efa08e65d98d903f10c9fe3e2e8a5cecb1e0
Demils <cdd2@hotmail.es>
2016-03-17 21:26:50 +0100

    Test Servlet -- Finish

--- src/main/java/TestServlet.java
+++ src/main/java/TestServlet.java
@@ -1,5 +1,4 @@
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,31 +8,17 @@ import java.io.PrintWriter;
 /**
  * Created by Cd on 17/03/2016.
  */
-@WebServlet("/HelloWorld")
-public class TestServlet {
-    // Extend HttpServlet class
-    public class HelloWorld extends HttpServlet {
 
-        private String message;
+public class TestServlet extends HttpServlet {
 
-        public void init() throws ServletException
-        {
-            // Do required initialization
-            message = "Hello World";
-        }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        // Set response content type
+        resp.setContentType("text/html");
 
-        public void doGet(HttpServletRequest request,
-                          HttpServletResponse response)
-                throws ServletException, IOException
-        {
-            response.setContentType("text/html");
-            PrintWriter printWriter  = response.getWriter();
-            printWriter.println("<h1>Hello World!</h1>");
-        }
-
-        public void destroy()
-        {
-            // do nothing.
-        }
+        // Actual logic goes here.
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>" + "Test" + "</h1>");
     }
 }
