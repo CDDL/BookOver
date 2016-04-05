2bdcaae422e723c97a84a57e746eb197ebb196a5
Demils <cdd2@hotmail.es>
2016-04-05 16:39:44 +0200

    Muchas cosas

--- src/main/java/servlets/authentication/RegisterServlet.java
+++ src/main/java/servlets/authentication/RegisterServlet.java
@@ -1,7 +1,18 @@
 package servlets.authentication;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 /**
  * Created by Demils on 17/03/2016.
  */
-public class RegisterServlet {
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
