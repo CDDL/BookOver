383ae1dfb30cd10c3f5e40f6b9c9935a4336f92a
Demils <cdd2@hotmail.es>
2016-04-07 18:27:03 +0200

    WebServices inicios

--- src/main/java/servlets/Hello.java
+++ src/main/java/servlets/Hello.java
@@ -6,6 +6,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import java.io.File;
+import java.io.InputStream;
 
 @Path("hello")
 public class Hello {
@@ -26,4 +28,11 @@ public class Hello {
     public String newMethod(@QueryParam("name") String name, @QueryParam("surname") String surname) {
         return name.toUpperCase() + "_" + surname.toUpperCase();
     }
+
+//    @GET
+//    @Path("testfichero")
+//    @Produces(MediaType.TEXT_HTML)
+//    public InputStream devolverHtml(){
+//        return new File("index.html");
+//    }
 }
