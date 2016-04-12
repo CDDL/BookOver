757d4e68db56d7b7575eb0f5afa692a42b2de79a
Trabajo <Trabajo>
2016-04-12 16:09:27 +0200

    anglar controller

---
+++ src/main/webapp/js/angular_app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Demils on 09/04/2016.
+ */
+
+var myApp = angular.module('BookOver',['ngAnimate', 'ui.bootstrap']);
+
+myApp.controller('ControladorNavBar', function() {
+    this.dipe = 'Hola!';
+});
