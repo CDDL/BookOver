2c06a3b85d86c5fa20051dd402ccec259034305a
Demils <al253917@uji.es>
2016-04-27 18:24:02 +0200

    Biti: Login Funcional!!

---
+++ src/main/java/modelo/datos/LoginData.java
@@ -0,0 +1,38 @@
+package modelo.datos;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * Created by Demils on 27/04/2016.
+ */
+
+@XmlRootElement
+@XmlType(propOrder = {"username", "password"})
+public class LoginData {
+    private String username;
+    private String password;
+
+    public LoginData() {}
+
+    public LoginData(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
