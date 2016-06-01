43100fc85e6d7cf788618d6258e54540c25fb7eb
serrano94 <al259368@uji.es>
2016-06-01 21:54:41 +0200

    libroeditar test done

--- src/main/test/java/servicios/LibroEditar.java
+++ src/main/test/java/servicios/LibroEditar.java
@@ -85,7 +85,7 @@ public class LibroEditar extends DatabaseTest{
         libroNuevo.setEsPrestable(false);
         libroNuevo.setEsIntercambiable(false);
         libroNuevo.setEsVendible(false);
-        Response libroResponse = WebClient.create(URI_APP_BASE + "/libro/nuevo").header("Authentication", token.getToken()).post(libroNuevo);
+        Response libroResponse = WebClient.create(URI_APP_BASE + "/libros").header("Authentication", token.getToken()).post(libroNuevo);
         Libro libro = libroResponse.readEntity(Libro.class);
 
 
@@ -125,7 +125,7 @@ public class LibroEditar extends DatabaseTest{
         libroNuevo.setEsPrestable(false);
         libroNuevo.setEsIntercambiable(false);
         libroNuevo.setEsVendible(false);
-        WebClient.create(URI_APP_BASE + "/libro/nuevo").header("Authentication", token.getToken()).post(libroNuevo);
+        WebClient.create(URI_APP_BASE + "/libros").header("Authentication", token.getToken()).post(libroNuevo);
 
         //WHEN
         usuarioEnDatabase = new Usuario();
@@ -141,9 +141,45 @@ public class LibroEditar extends DatabaseTest{
         token = loginResponse.readEntity(Token.class);
 
         libroNuevo.setTitulo("Te he hackeado el libro");
-        Response response = WebClient.create(URI_APP_BASE + "/libro/editar").header("Authentication", token.getToken()).put(libroNuevo);
+        Response response = WebClient.create(URI_APP_BASE + "libro/"+ libroNuevo.getId()).header("Authentication", token.getToken()).put(libroNuevo);
 
         //ESPERADO
         assertThat(response.getStatusInfo().getStatusCode(), is(403));
+    }
+
+    @Test
+    public void editarLibro_libroinexistente_response404(){
+        //PRE
+        Usuario usuarioEnDatabase = new Usuario();
+        usuarioEnDatabase.setUsername("test");
+        usuarioEnDatabase.setEmail("test@test.com");
+        usuarioEnDatabase.setUbicacion("madrid");
+        usuarioEnDatabase.setPassword("139123");
+        WebClient.create(URI_APP_BASE + "register").post(usuarioEnDatabase);
+
+        DataLogin loginData = new Usuario();
+        loginData.setUsername("test");
+        loginData.setPassword("139123");
+        Response loginResponse = WebClient.create(URI_APP_BASE + "login").post(loginData);
+        Token token = loginResponse.readEntity(Token.class);
+
+        Libro libroNuevo = new Libro();
+        libroNuevo.setTitulo("Libro custom");
+        libroNuevo.setAutor("yoMismo");
+        libroNuevo.setEditorial("Editorial rara");
+        libroNuevo.setIsbn("ISBNRANDOM");
+        libroNuevo.setEstado("Roto");
+        libroNuevo.setInfoAdicional("Lo encontre en la calle");
+        libroNuevo.setEsPrestable(false);
+        libroNuevo.setEsIntercambiable(false);
+        libroNuevo.setEsVendible(false);
+
+        //WHEN
+        Response response = WebClient.create(URI_APP_BASE + "libro/"+ libroNuevo.getId()).header("Authentication", token.getToken()).put(libroNuevo);
+
+        //ESPERADO
+        assertThat(response.getStatusInfo().getStatusCode(), is(404));
+
+
     }
 }
