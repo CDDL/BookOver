9d9c2201cf2832accd69f4fec8dfa677b6459fa1
serrano94 <al259368@uji.es>
2016-06-07 02:20:55 +0200

    eye of mind true

--- src/main/java/controladores/ControladorLibro.java
+++ src/main/java/controladores/ControladorLibro.java
@@ -39,7 +39,7 @@ public class ControladorLibro implements IControllerLibro {
         miLibro.setFotos(libro.getFotos());
         miLibro.setInfoAdicional(libro.getInfoAdicional());
         miLibro.setIsbn(libro.getIsbn());
-        miLibro.setVisible(libro.isVisible());
+        //miLibro.setVisible(libro.isVisible());
         miLibro.setPrecio(libro.getPrecio());
         mDataLibro.actualizar(miLibro);
 
